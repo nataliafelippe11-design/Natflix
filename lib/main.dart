4debb7c6a997481ef96a2904a79a5dff9c61e0d2
nataliafelippe11-design <nataliafelippe11@gmail.com>
2026-07-01 16:02:50 -0300

    Terminei o trabalho. Fiz a estilização e dei retoques finais

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,45 @@ class MyApp extends StatelessWidget {
 
       debugShowCheckedModeBanner: false,
 
-      home: TelaLogin(),
+        theme: ThemeData(
+
+      scaffoldBackgroundColor: const Color.fromARGB(255, 175, 175, 175),
+
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Color.fromARGB(255, 171, 13, 2),
+              foregroundColor: Colors.white,
+              centerTitle: true,
+            ),
+
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+              ),
+            ),
+
+            cardTheme: CardThemeData(
+              color: const Color.fromARGB(255, 0, 114, 13),
+              elevation: 6,
+            ),
+
+            textTheme: const TextTheme(
+
+              titleLarge: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+
+              bodyMedium: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          
+            fontFamily: "BebasNeue",
+  ),
+
+  home: TelaLogin(),
     );
   }
 }
